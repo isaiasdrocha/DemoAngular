270b051a586c9249f419cb85873858b87de6e2cc
Isaias D Rocha Jr <isaiasdrocha@gmail.com>
2022-01-18 13:51:32 -0300

    Add new example page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DemoAngular';
+
+  transferencia: any;
+  //valor: number;
+  //destino: number;
+
+
+  transferir($event:any){
+    console.log("Cheguei");
+    console.log($event);
+    //this.valor = $event.valor;
+    //this.destino = $event.destino;
+    this.transferencia = $event;
+  }
+
 }
---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-trasferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor:number;
+  destino:number;
+
+  transferir() {
+    console.log('Solicitado nova transferencia');
+    console.log('Valor', this.valor);
+    console.log('Destino', this.destino);
+
+    const valorEmitir = {
+      valor: this.valor,
+      destino: this.destino
+    };
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+
+}
